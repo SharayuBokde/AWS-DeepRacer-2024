6eb8ae38cfcfc4645a9116e29ddbcaf48d1591eb
Sharayu Bokde <bokde.sharayu@gmail.com>
2024-07-10 20:50:45 +0530

    Update reward_function.py

--- reward_function.py
+++ reward_function.py
@@ -1,7 +1,7 @@
 import math
 
 # Calculate heading change between consecutive waypoints
-def calculate_heading_change(waypoints, closest_waypoints):
+def calculate_heading_change(waypoints, closest_waypoints, heading):
     next_waypoint = waypoints[closest_waypoints[1]]
     prev_waypoint = waypoints[closest_waypoints[0]]
 
@@ -36,6 +36,8 @@ def reward_function(params):
     # Penalize if the car goes off track
     if not all_wheels_on_track:
         return reward  # Reward will be 1e-3 if the car is off track
+    else:
+        reward += 10
     
     # Reward car for staying close to the center line
     marker_1 = 0.1 * track_width
@@ -56,24 +58,24 @@ def reward_function(params):
         reward += 10.0  # bonus for completing the lap
     
     # Reward for maintaining an appropriate speed
-    SPEED_THRESHOLD = 1.0
+    SPEED_THRESHOLD = 2.0
     if speed > SPEED_THRESHOLD:
-        reward += 0.5  # higher reward for driving faster
+        reward += 1.0  # higher reward for driving faster
 
     # Penalize for steering too much to avoid zig-zagging
-    ABS_STEERING_THRESHOLD = 15
+    ABS_STEERING_THRESHOLD = 10
     if steering_angle > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
+        reward *= 0.7
         
     # Determine if the car is approaching a steep curve
     HEADING_THRESHOLD = 15  # Define a threshold for what constitutes a steep curve
-    heading_change = calculate_heading_change(waypoints, closest_waypoints)
+    heading_change = calculate_heading_change(waypoints, closest_waypoints, heading)
     is_steep_curve = heading_change > HEADING_THRESHOLD
 
     # Adjust rewards based on the curve
     if is_steep_curve:
         # Reduce speed and reward accordingly
-        SPEED_THRESHOLD = 1.0
+        SPEED_THRESHOLD = 2.0
         if speed < SPEED_THRESHOLD:
             reward += 2.0  # Higher reward for reducing speed at steep curve
         else:
@@ -88,11 +90,11 @@ def reward_function(params):
 
         # Encourage optimal path through the curve
         if distance_from_center < marker_2:
-            reward += 1.0  # Bonus for maintaining optimal trajectory
+            reward += 2.0  # Bonus for maintaining optimal trajectory
     else:
         # General speed incentive on non-steep sections
         SPEED_THRESHOLD = 2.0
-        if speed > SPEED_THRESHOLD:
+        if speed >= SPEED_THRESHOLD:
             reward += 0.5  # reward for maintaining high speed
             
     return float(reward)
